a2e23150ab87b67966e32fc5ca8c84be40aca4cf
mazohu <35633008+mazohu@users.noreply.github.com>
2022-10-08 18:38:14 -0400

    Added static site files

---
+++ scrolling.js
@@ -0,0 +1,6 @@
+// JavaScript function for scrolling smoothly to target
+function smoothScroll(target) {
+	target.scrollIntoView(false);
+}
+
+document.body.scrollTop = document.documentElement.scrollTop = 0;
